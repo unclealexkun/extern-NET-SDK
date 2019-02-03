5adf251940aa6f61a19292e963fea9c47c114737
UncleAlexKun <raisin27@mail.ru>
2019-02-03 20:00:31 +0400

    Полноценная подготовка проекта
    Создал UnitTest, Запускатор и сам модуль библиотеки

---
+++ extern-NET-SDK/TestExternSharpSDK/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestExternSharpSDK")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestExternSharpSDK")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f1d5fbc5-6872-4039-a42a-1e44733da3da")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ extern-NET-SDK/TestExternSharpSDK/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestExternSharpSDK
+{
+	[TestClass]
+	public class UnitTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
